db015d799c2815d7ee2f94e648577c353e16331e
ichika-m <136573809+ichika-m@users.noreply.github.com>
2023-07-31 08:22:50 +0000

    [Finish]Bookers2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+
   devise_for :users
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+  resources :users, only: [:index, :show, :edit, :update]
+
+
   root to: 'homes#top'
+  get "/home/about" =>"homes#about",as: '/home/about'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
